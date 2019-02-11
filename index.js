f503d9eea8a41f6966f0cd4a81ec7d43c46a2288
fedysalah <fedy.salah@serli.com>
2019-02-11 19:29:47 +0100

    third draft

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname,  'public')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
